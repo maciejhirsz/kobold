aa4f58d0c989a94934cdaaaa5214b0cfab0cc1ff
Maciej Hirsz <hello@maciej.codes>
2021-08-02 10:43:08 +0200

    Use itoa/dtoa, no need to allocate to print ints and floats

--- sketch/src/value.rs
+++ sketch/src/value.rs
@@ -1,17 +1,17 @@
 use crate::traits::{Html, Mountable, Update};
 use crate::util;
 use web_sys::Node;
+use std::str;
 
 pub struct RenderedValue<T> {
-	value: T,
-	buf: String,
-	node: Node,
+    value: T,
+    node: Node,
 }
 
 impl<T> Mountable for RenderedValue<T> {
-	fn node(&self) -> &Node {
-		&self.node
-	}
+    fn node(&self) -> &Node {
+        &self.node
+    }
 }
 
 fn bool_to_str(b: bool) -> &'static str {
@@ -25,21 +25,60 @@ impl Html for bool {
         let node = util::__sketch_text_node(bool_to_str(self));
 
         RenderedValue {
-        	value: self,
-        	buf: String::new(),
-        	node,
+            value: self,
+            node,
         }
     }
 }
 
 impl Update<bool> for RenderedValue<bool> {
     fn update(&mut self, new: bool) {
-    	if self.value != new {
-    		self.value = new;
+        if self.value != new {
+            self.value = new;
 
-    		util::__sketch_update_text(&self.node, bool_to_str(self.value));
-    	}
+            util::__sketch_update_text(&self.node, bool_to_str(self.value));
+        }
     }
+}
+
+macro_rules! impl_render_num {
+    ($lib:ident, $($t:ty),*) => {
+        $(
+            impl Html for $t {
+                type Rendered = RenderedValue<$t>;
+
+                fn render(self) -> Self::Rendered {
+                    let mut buf = [0_u8; 20];
+
+                    let n = $lib::write(&mut buf[..], self).unwrap_or_else(|_| 0);
+                    let node = util::__sketch_text_node(unsafe {
+                        str::from_utf8_unchecked(&buf[..n])
+                    });
+
+                    RenderedValue {
+                        value: self,
+                        node,
+                    }
+                }
+            }
+
+            impl Update<$t> for RenderedValue<$t> {
+                fn update(&mut self, new: $t) {
+                    if self.value != new {
+                        self.value = new;
+
+                        let mut buf = [0_u8; 20];
+
+                        let n = $lib::write(&mut buf[..], new).unwrap_or_else(|_| 0);
+
+                        util::__sketch_update_text(&self.node, unsafe {
+                            str::from_utf8_unchecked(&buf[..n])
+                        });
+                    }
+                }
+            }
+        )*
+    };
 }
 
 macro_rules! impl_render_value {
@@ -53,31 +92,28 @@ macro_rules! impl_render_value {
                     let node = util::__sketch_text_node(&buf);
 
                     RenderedValue {
-                    	value: self,
-                    	buf,
-                    	node,
+                        value: self,
+                        node,
                     }
                 }
             }
 
             impl Update<$t> for RenderedValue<$t> {
                 fn update(&mut self, new: $t) {
-                	use std::fmt::Write;
 
-                	if self.value != new {
-                		self.value = new;
+                    if self.value != new {
+                        self.value = new;
 
-                		self.buf.clear();
+                        let buf = new.to_string();
 
-                		// Writing to String is infallible
-                		let _ = write!(&mut self.buf, "{}", new);
-
-                		util::__sketch_update_text(&self.node, &self.buf);
-                	}
+                        util::__sketch_update_text(&self.node, &buf);
+                    }
                 }
             }
         )*
     };
 }
 
-impl_render_value!(u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64);
+impl_render_num!(itoa, u8, u16, u32, u64, i8, i16, i32, i64, usize, isize);
+impl_render_num!(dtoa, f32, f64);
+impl_render_value!(u128, i128);
