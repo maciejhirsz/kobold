523f5306d92c14c41560958fb59e1b401f46338f
Maciej Hirsz <hello@maciej.codes>
2022-06-17 22:20:41 +0100

    Working on generic-ish link

--- crates/kobold_experiment/src/lib.rs
+++ crates/kobold_experiment/src/lib.rs
@@ -28,17 +28,17 @@ pub trait Mountable: 'static {
 
 pub type ShouldRender = bool;
 
-pub trait EventedComponent: Sized {
-    type State: Render;
+// pub trait EventedComponent: Sized {
+//     type State: Render;
 
-    fn init(self, link: Link<Self::State>) -> Self::State;
+//     fn init(self, link: Link<Self::State>) -> Self::State;
 
-    fn update(self, link: Link<Self::State>, state: &mut Self::State) -> ShouldRender {
-        *state = self.init(link);
+//     fn update(self, link: Link<Self::State>, state: &mut Self::State) -> ShouldRender {
+//         *state = self.init(link);
 
-        true
-    }
-}
+//         true
+//     }
+// }
 
 pub trait Render: 'static {
     type Out: Html;
@@ -46,13 +46,50 @@ pub trait Render: 'static {
     fn render(&self) -> Self::Out;
 }
 
-pub struct Link<'a, S: Render> {
-    state: &'a Weak<RefCell<S>>,
+// pub struct Link<'a, S: Render> {
+//     state: &'a Weak<RefCell<S>>,
+// }
+
+// impl<S: Render> Link<'_, S> {
+//     pub fn link(&self, f: impl Fn(&mut S) -> ShouldRender + 'static) -> Closure {
+//         let state = self.state.clone();
+
+//         Closure(Box::new(move || {
+//             if let Some(rc) = state.upgrade() {
+//                 let mut state = rc.borrow_mut();
+
+//                 if f(&mut state) {
+//                     state.render();
+//                 }
+//             }
+//         }))
+//     }
+// }
+
+pub struct Closure(Box<dyn FnMut()>);
+
+trait Link<T> {
+    type Hosted;
+
+    fn host(item: T) -> Self::Hosted;
 }
 
-impl<S: Render> Link<'_, S> {
-    pub fn link(&self, f: impl Fn(&mut S) -> ShouldRender + 'static) -> Closure {
-        let state = self.state.clone();
+struct Pure;
+
+impl<T> Link<T> for Pure {
+    type Hosted = T;
+
+    fn host(item: T) -> Self::Hosted {
+        item
+    }
+}
+
+#[repr(transparent)]
+struct Evented<T>(Weak<RefCell<T>>);
+
+impl<T: Render> Evented<T> {
+    pub fn link(&self, f: impl Fn(&mut T) -> ShouldRender + 'static) -> Closure {
+        let state = self.0.clone();
 
         Closure(Box::new(move || {
             if let Some(rc) = state.upgrade() {
@@ -66,108 +103,22 @@ impl<S: Render> Link<'_, S> {
     }
 }
 
-pub struct Closure(Box<dyn FnMut()>);
+impl<T> Link<T> for Evented<T> {
+    type Hosted = Rc<RefCell<T>>;
+
+    fn host(item: T) -> Self::Hosted {
+        Rc::new(RefCell::new(item))
+    }
+}
 
 trait Component: Sized {
     type Built: Mountable;
 
-    // fn create(self) -> Self::Built;
+    type Link: Link<Self>;
 
-    // fn update(self, _built: &mut Self::Built);
-}
+    fn create(self, _link: &Self::Link) -> <Self::Link as Link<Self>>::Hosted;
 
-trait SimpleComponent: Sized {
-    type Out: Html;
-
-    fn render(self) -> Self::Out;
-
-    fn create(self) -> <Self::Out as Html>::Built {
-        self.render().build()
-    }
-
-    fn update(self, built: &mut <Self::Out as Html>::Built) {
-        self.render().update(built);
+    fn update(self, link: &Self::Link, built: &mut <Self::Link as Link<Self>>::Hosted) {
+        *built = self.create(link);
     }
 }
-
-struct EventedBuilt<State: Render> {
-    built_html: <State::Out as Html>::Built,
-    state: State,
-}
-
-impl<State: Render> Mountable for EventedBuilt<State> {
-    fn js(&self) -> &JsValue {
-        self.built_html.js()
-    }
-
-    fn mount(&self, parent: &Node) {
-        self.built_html.mount(parent);
-    }
-
-    fn unmount(&self, parent: &Node) {
-        self.built_html.unmount(parent);
-    }
-}
-
-impl<E> Component for E
-where
-    E: EventedComponent,
-{
-    type Built = EventedBuilt<E::State>;
-
-    // fn create(self) -> Self::Built {
-
-    // }
-}
-
-// pub trait Component: Sized {
-//     type Properties;
-
-//     fn create(props: Self::Properties) -> Self;
-
-//     fn update(&mut self, new: Self::Properties) -> ShouldRender;
-// }
-
-// struct Evented<Out> {
-//     Out
-// }
-
-// impl<E> Component for E
-// where
-//     E: EventedComponent,
-// {
-//     type Out = <E::State as Render>::Out;
-
-//     fn render(self) -> Self::Out {
-//         let state = Rc::new_cyclic(|state| RefCell::new(self.init(Link { state })));
-
-//         let state = state.borrow();
-
-//         state.render()
-//     }
-// }
-
-// struct MyState;
-
-
-// impl State for MyState {
-//     #[kobold]
-//     fn render(&self) -> impl Html {
-//         panic!()
-//     }
-// }
-
-// #[kobold]
-// fn Modal(name: &str) -> impl Html {
-//     <p>{name}</p>
-// }
-
-// struct Modal<'a> {
-//     name: &'a str,
-// }
-
-// kobold! {
-//     impl Component for Modal<'_> {
-//         fn render()
-//     }
-// }
