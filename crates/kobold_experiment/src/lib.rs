3d29cae2e9cbe53b3704fadfd4a8c8d4f562f502
Maciej Hirsz <hello@maciej.codes>
2022-06-18 18:08:59 +0100

    Working(?) stateful impl

--- crates/kobold_experiment/src/lib.rs
+++ crates/kobold_experiment/src/lib.rs
@@ -1,18 +1,17 @@
-use std::borrow::{BorrowMut, Borrow};
-use std::cell::RefCell;
-use std::rc::{Rc, Weak};
-
 use wasm_bindgen::JsValue;
 use web_sys::Node;
 
 mod util;
+mod stateful;
+
+pub use stateful::stateful;
 
 pub trait Html: Sized {
-    type Built: Mountable;
+    type Product: Mountable;
 
-    fn build(self) -> Self::Built;
+    fn build(self) -> Self::Product;
 
-    fn update(self, built: &mut Self::Built);
+    fn update(self, p: &mut Self::Product);
 }
 
 pub trait Mountable: 'static {
@@ -29,172 +28,6 @@ pub trait Mountable: 'static {
 
 pub type ShouldRender = bool;
 
-// pub trait EventedComponent: Sized {
-//     type State: Render;
-
-//     fn init(self, link: Link<Self::State>) -> Self::State;
-
-//     fn update(self, link: Link<Self::State>, state: &mut Self::State) -> ShouldRender {
-//         *state = self.init(link);
-
-//         true
-//     }
-// }
-
-pub trait Render: 'static {
-    type Out: Html;
-
-    fn render(&self) -> Self::Out;
-}
-
-// pub struct Link<'a, S: Render> {
-//     state: &'a Weak<RefCell<S>>,
-// }
-
-// impl<S: Render> Link<'_, S> {
-//     pub fn link(&self, f: impl Fn(&mut S) -> ShouldRender + 'static) -> Closure {
-//         let state = self.state.clone();
-
-//         Closure(Box::new(move || {
-//             if let Some(rc) = state.upgrade() {
-//                 let mut state = rc.borrow_mut();
-
-//                 if f(&mut state) {
-//                     state.render();
-//                 }
-//             }
-//         }))
-//     }
-// }
-
-struct Inner<S, H: Html> {
-    state: RefCell<S>,
-    product: RefCell<H::Built>,
-    render: fn(&S, Link<S, H>) -> H,
-}
-
-pub struct Stateful<S, H: Html> {
-    inner: Rc<Inner<S, H>>,
-}
-
-pub struct StatefulProduct<S, H: Html> {
-    inner: Rc<Inner<S, H>>,
-    js: JsValue,
-}
-
-impl<S, H: Html> Html for Stateful<S, H> {
-    type Built = StatefulProduct<S, H>;
-
-    fn build(self) -> Self::Built {
-        let inner = self.inner;
-        let js = inner.product.borrow_mut().js().clone();
-
-        StatefulProduct { inner, js }
-    }
-
-    fn update(self, built: &mut Self::Built) {
-
-    }
-}
-
-impl<S, H> Mountable for StatefulProduct<S, H>
-where
-    H: Html,
-{
-    fn js(&self) -> &JsValue {
-        &self.js
-    }
-}
-
-#[derive(Copy)]
-struct Link<'a, S, H: Html> {
-    inner: &'a Weak<Inner<S, H>>,
-}
-
-impl<S, H: Html> Clone for Link<'_, S, H> {
-    fn clone(&self) -> Self {
-        Link {
-            inner: self.inner,
-        }
-    }
-}
-
-fn stateful<S, H: Html>(state: S, render: fn(&S, Link<S, H>) -> H) -> Stateful<S, H> {
-    let inner = Rc::new_cyclic(move |inner| {
-        let product = render(&state, Link { inner }).build();
-
-        Inner {
-            state: RefCell::new(state),
-            product: RefCell::new(product),
-            render,
-        }
-    });
-
-    let js = inner.product.borrow().js().clone();
-
-    Stateful { inner, js }
-}
-
-impl<S, H: Html> Link<'_, S, H> {
-    fn bind(&self, fun: fn(&mut S)) -> Callback<S, H> {
-        Callback {
-            fun,
-            link: self.clone(),
-        }
-    }
-}
-
-pub struct Callback<'a, S, H: Html> {
-    fun: fn(&mut S),
-    link: Link<'a, S, H>,
-}
-
-pub struct CallbackProduct<S> {
-    fun: fn(&mut S),
-    closure: Box<dyn FnMut()>,
-}
-
-impl<S, H: Html> Html for Callback<'_, S, H>
-where
-    S: 'static,
-{
-    type Built = CallbackProduct<S>;
-
-    fn build(self) -> Self::Built {
-        let weak = self.link.inner.clone();
-        let fun = self.fun;
-
-        CallbackProduct {
-            fun,
-            closure: Box::new(move || {
-                // if let Some(rc) = weak.upgrade() {
-                //     fun(&mut rc.state.borrow_mut());
-
-                //     let html = (rc.render)(&rc.state.borrow(), Link { inner: &weak });
-
-                //     let mut old = rc.product.borrow_mut();
-
-                //     if let Some(old) = &mut *old {
-                //         html.update(old);
-                //     }
-                // }
-            })
-        }
-    }
-
-    fn update(self, built: &mut Self::Built) {
-        // TODO: update closure if fun has changed.
-    }
-}
-
-impl<S> Mountable for CallbackProduct<S>
-where
-    S: 'static,
-{
-    fn js(&self) -> &JsValue {
-        unimplemented!()
-    }
-}
 
 struct Counter {
     n: i32,
@@ -212,13 +45,13 @@ impl Counter {
 }
 
 impl Html for i32 {
-    type Built = i32;
+    type Product = i32;
 
-    fn build(self) -> Self::Built {
+    fn build(self) -> Self::Product {
         self
     }
 
-    fn update(self, built: &mut Self::Built) {
+    fn update(self, built: &mut Self::Product) {
         *built = self;
     }
 }
---
+++ crates/kobold_experiment/src/stateful.rs
@@ -0,0 +1,160 @@
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
+use wasm_bindgen::JsValue;
+
+use crate::{Html, Mountable};
+
+pub trait ShouldRender {
+    fn should_render(self) -> bool;
+}
+
+impl ShouldRender for () {
+    fn should_render(self) -> bool {
+        true
+    }
+}
+
+impl ShouldRender for bool {
+    fn should_render(self) -> bool {
+        self
+    }
+}
+
+pub struct Stateful<S, H: Html> {
+    state: S,
+    render: fn(&S, &Link<S, H::Product>) -> H,
+}
+
+pub fn stateful<S, H>(state: S, render: fn(&S, &Link<S, H::Product>) -> H) -> Stateful<S, H>
+where
+    H: Html,
+{
+    Stateful { state, render }
+}
+
+/// Magic wrapper for render function that allows us to store it with 'static
+/// lifetime without the lifetime on return type getting in the way
+#[derive(Clone, Copy)]
+struct RenderFn(usize);
+
+impl RenderFn {
+    fn new<S, H: Html>(render: fn(&S, &Link<S, H::Product>) -> H) -> Self {
+        RenderFn(render as usize)
+    }
+
+    unsafe fn cast<S, H: Html>(self) -> fn(&S, &Link<S, H::Product>) -> H {
+        unsafe { std::mem::transmute(self.0) }
+    }
+}
+
+struct Inner<S, P> {
+    state: RefCell<S>,
+    product: RefCell<P>,
+    render: RenderFn,
+    link: Link<S, P>,
+    update: fn(RenderFn, &Link<S, P>),
+}
+
+impl<S, P> Inner<S, P> {
+    fn update(&self) {
+        (self.update)(self.render, &self.link)
+    }
+}
+
+pub struct StatefulProduct<S, P> {
+    inner: Rc<Inner<S, P>>,
+    js: JsValue,
+}
+
+pub struct Link<S, P> {
+    inner: Weak<Inner<S, P>>,
+}
+
+impl<S, P> Link<S, P>
+where
+    S: 'static,
+    P: 'static,
+{
+    pub fn bind<F, A>(&self, f: F) -> Box<dyn FnMut() + 'static>
+    where
+        F: Fn(&mut S) -> A + 'static,
+        A: ShouldRender,
+    {
+        let link = self.clone();
+
+        Box::new(move || {
+            if let Some(rc) = link.inner.upgrade() {
+                if f(&mut rc.state.borrow_mut()).should_render() {
+                    rc.update();
+                }
+            }
+        })
+    }
+}
+
+impl<S, P> Clone for Link<S, P> {
+    fn clone(&self) -> Self {
+        Link {
+            inner: self.inner.clone(),
+        }
+    }
+}
+
+impl<S, H> Html for Stateful<S, H>
+where
+    S: 'static,
+    H: Html,
+{
+    type Product = StatefulProduct<S, H::Product>;
+
+    fn build(self) -> Self::Product {
+        let inner = Rc::new_cyclic(move |inner| {
+            let link = Link {
+                inner: inner.clone(),
+            };
+            let product = (self.render)(&self.state, &link).build();
+
+            let render = RenderFn::new(self.render);
+
+            Inner {
+                state: RefCell::new(self.state),
+                product: RefCell::new(product),
+                render,
+                link: link.clone(),
+                update: |render, link| {
+                    // Safety: this is safe as long as `S` and `H` are the same types that
+                    // were used to create this `RenderFn` instance.
+                    let render = unsafe { render.cast::<S, H>() };
+
+                    if let Some(inner) = link.inner.upgrade() {
+                        (render)(&inner.state.borrow(), &link)
+                            .update(&mut inner.product.borrow_mut());
+                    }
+                },
+            }
+        });
+
+        let js = inner.product.borrow().js().clone();
+
+        StatefulProduct { inner, js }
+    }
+
+    fn update(self, p: &mut Self::Product) {
+        *p.inner.state.borrow_mut() = self.state;
+
+        // p.inner.update();
+        (self.render)(&p.inner.state.borrow(), &p.inner.link);
+        // (p.inner.update)(p.inner.render, &p.inner.link);
+    }
+}
+
+impl<S, P> Mountable for StatefulProduct<S, P>
+where
+    S: 'static,
+    P: Mountable,
+{
+    fn js(&self) -> &JsValue {
+        &self.js
+    }
+}
