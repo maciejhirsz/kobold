7868c861908df5fc9c3832e754490d701e3fce40
Maciej Hirsz <hello@maciej.codes>
2022-06-18 12:52:47 +0100

    More experiments with functional stateful components

--- crates/kobold_experiment/src/lib.rs
+++ crates/kobold_experiment/src/lib.rs
@@ -1,3 +1,4 @@
+use std::borrow::{BorrowMut, Borrow};
 use std::cell::RefCell;
 use std::rc::{Rc, Weak};
 
@@ -66,59 +67,155 @@ pub trait Render: 'static {
 //     }
 // }
 
-pub struct Closure(Box<dyn FnMut()>);
-
-trait Link<T> {
-    type Hosted;
-
-    fn host(item: T) -> Self::Hosted;
+struct Inner<S, H: Html> {
+    state: RefCell<S>,
+    product: RefCell<H::Built>,
+    render: fn(&S, Link<S, H>) -> H,
 }
 
-struct Pure;
+pub struct Stateful<S, H: Html> {
+    inner: Rc<Inner<S, H>>,
+    js: JsValue,
+}
 
-impl<T> Link<T> for Pure {
-    type Hosted = T;
+impl<S, H: Html> Html for Stateful<S, H> {
+    type Built = Self;
 
-    fn host(item: T) -> Self::Hosted {
-        item
+    fn build(self) -> Self::Built {
+        self
+    }
+
+    fn update(self, built: &mut Self::Built) {
+
     }
 }
 
-#[repr(transparent)]
-struct Evented<T>(Weak<RefCell<T>>);
-
-impl<T: Render> Evented<T> {
-    pub fn link(&self, f: impl Fn(&mut T) -> ShouldRender + 'static) -> Closure {
-        let state = self.0.clone();
-
-        Closure(Box::new(move || {
-            if let Some(rc) = state.upgrade() {
-                let mut state = rc.borrow_mut();
-
-                if f(&mut state) {
-                    state.render();
-                }
-            }
-        }))
+impl<S, H> Mountable for Stateful<S, H>
+where
+    H: Html,
+{
+    fn js(&self) -> &JsValue {
+        &self.js
     }
 }
 
-impl<T> Link<T> for Evented<T> {
-    type Hosted = Rc<RefCell<T>>;
+#[derive(Copy)]
+struct Link<'a, S, H: Html> {
+    inner: &'a Weak<Inner<S, H>>,
+}
 
-    fn host(item: T) -> Self::Hosted {
-        Rc::new(RefCell::new(item))
+impl<S, H: Html> Clone for Link<'_, S, H> {
+    fn clone(&self) -> Self {
+        Link {
+            inner: self.inner,
+        }
     }
 }
 
-trait Component: Sized {
-    type Built: Mountable;
+fn stateful<S, H: Html>(state: S, render: fn(&S, Link<S, H>) -> H) -> Stateful<S, H> {
+    let inner = Rc::new_cyclic(move |inner| {
+        let product = render(&state, Link { inner }).build();
 
-    type Link: Link<Self>;
+        Inner {
+            state: RefCell::new(state),
+            product: RefCell::new(product),
+            render,
+        }
+    });
 
-    fn create(self, _link: &Self::Link) -> <Self::Link as Link<Self>>::Hosted;
+    Stateful { inner }
+}
 
-    fn update(self, link: &Self::Link, built: &mut <Self::Link as Link<Self>>::Hosted) {
-        *built = self.create(link);
+impl<S, H: Html> Link<'_, S, H> {
+    fn bind(&self, fun: fn(&mut S)) -> Callback<S, H> {
+        Callback {
+            fun,
+            link: self.clone(),
+        }
+    }
+}
+
+pub struct Callback<'a, S, H: Html> {
+    fun: fn(&mut S),
+    link: Link<'a, S, H>,
+}
+
+pub struct CallbackProduct<S> {
+    fun: fn(&mut S),
+    closure: Box<dyn FnMut()>,
+}
+
+impl<S, H: Html> Html for Callback<'_, S, H>
+where
+    S: 'static,
+{
+    type Built = CallbackProduct<S>;
+
+    fn build(self) -> Self::Built {
+        let weak = self.link.inner.clone();
+        let fun = self.fun;
+
+        CallbackProduct {
+            fun,
+            closure: Box::new(move || {
+                // if let Some(rc) = weak.upgrade() {
+                //     fun(&mut rc.state.borrow_mut());
+
+                //     let html = (rc.render)(&rc.state.borrow(), Link { inner: &weak });
+
+                //     let mut old = rc.product.borrow_mut();
+
+                //     if let Some(old) = &mut *old {
+                //         html.update(old);
+                //     }
+                // }
+            })
+        }
+    }
+
+    fn update(self, built: &mut Self::Built) {
+        // TODO: update closure if fun has changed.
+    }
+}
+
+impl<S> Mountable for CallbackProduct<S>
+where
+    S: 'static,
+{
+    fn js(&self) -> &JsValue {
+        unimplemented!()
+    }
+}
+
+struct Counter {
+    n: i32,
+}
+
+impl Counter {
+    pub fn render(&self) -> impl Html {
+        stateful(self.n, |state, link| {
+            let inc = link.bind(|n| *n += 1);
+            let dec = link.bind(|n| *n -= 1);
+
+            *state
+        })
+    }
+}
+
+impl Html for i32 {
+    type Built = i32;
+
+    fn build(self) -> Self::Built {
+        self
+    }
+
+    fn update(self, built: &mut Self::Built) {
+        *built = self;
+    }
+}
+
+impl Mountable for i32 {
+    fn js(&self) -> &JsValue {
+        panic!()
     }
 }
