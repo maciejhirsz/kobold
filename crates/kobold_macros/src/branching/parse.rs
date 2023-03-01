bb2d5153420b4e10769f9cb52a0a4e9eac43a031
Maciej Hirsz <hello@maciej.codes>
2023-03-01 08:35:08 +0000

    Rework the control flow parser to an FSM

--- crates/kobold_macros/src/branching/parse.rs
+++ crates/kobold_macros/src/branching/parse.rs
@@ -47,9 +47,18 @@ impl CodeBuilder {
     }
 }
 
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum Token {
+    /// The `html` identifier
     Html,
+    /// The `if` keyword
     If,
+    /// The `else` keyword
+    Else,
+    /// The `{ ... }` block
+    Block,
+    /// Any `Group` other than `{ ... }` block
+    Group,
 }
 
 impl Token {
@@ -57,6 +66,16 @@ impl Token {
         match ident {
             "html" => Some(Token::Html),
             "if" => Some(Token::If),
+            "else" => Some(Token::Else),
+            _ => None,
+        }
+    }
+
+    fn get(tt: &TokenTree) -> Option<Token> {
+        match tt {
+            TokenTree::Ident(ident) => ident.with_str(Token::from_str),
+            tt if tt.is('{') => Some(Token::Block),
+            TokenTree::Group(_) => Some(Token::Group),
             _ => None,
         }
     }
@@ -64,100 +83,112 @@ impl Token {
 
 type Branches<'a> = Option<&'a Rc<Cell<u8>>>;
 
-struct ShallowParser<'a> {
-    stream: &'a mut ParseStream,
-}
-
-impl Iterator for ShallowParser<'_> {
-    type Item = TokenTree;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.stream.allow('{') {
-            return None;
-        }
-
-        self.stream.next()
-    }
-}
-
 fn parse_code(stream: &mut ParseStream, scope: Branches) -> Result<Vec<Code>, ParseError> {
     let mut code = CodeBuilder::default();
 
+    enum Mode {
+        /// Next token must match state
+        Eager,
+
+        /// Allow other tokens until a match
+        Lazy,
+    }
+
+    struct State(Token, Mode, Rc<Cell<u8>>);
+
+    let mut states = Vec::<State>::new();
+
     while let Some(tt) = stream.next() {
-        match tt {
-            TokenTree::Ident(ref ident) => {
-                match ident.with_str(Token::from_str) {
-                    Some(Token::Html) => {
-                        let mut maybe_html = ArrayVec::<_, 3>::new();
-
-                        maybe_html.push(tt);
-                        maybe_html.extend(
-                            ['!', '{']
-                                .into_iter()
-                                .map_while(|p| stream.allow_consume(p)),
-                        );
-
-                        match maybe_html.into_inner() {
-                            Ok(html) => {
-                                let branches = scope.map(Clone::clone);
-
-                                code.push(Code::Html(Html::new(
-                                    html.into_iter().collect(),
-                                    branches,
-                                )));
-                            }
-                            Err(tokens) => code.extend(tokens),
+        let token = match Token::get(&tt) {
+            Some(token) => token,
+            None => {
+                code.collect(tt);
+                continue;
+            }
+        };
+        let state = match states.last() {
+            Some(State(expect, mode, _)) => {
+                if *expect == token {
+                    // Match! Get owned state
+                    states.pop()
+                } else {
+                    match mode {
+                        // No match, discard state
+                        Mode::Eager => {
+                            let _ = states.pop();
                         }
-
-                        continue;
+                        // No match, retain state
+                        Mode::Lazy => (),
                     }
-                    Some(Token::If) => {
-                        let scope = Rc::new(Cell::new(0));
-                        let scope = Some(&scope);
+                    None
+                }
+            }
+            _ => None,
+        };
 
-                        code.collect(tt);
-                        code.extend(ShallowParser { stream });
+        match token {
+            Token::Html => {
+                let mut maybe_html = ArrayVec::<_, 3>::new();
 
-                        if let TokenTree::Group(group) = stream.expect('{')? {
-                            code.push(Code::Nested(Nested::parse(group, scope)?));
-                        }
+                maybe_html.push(tt);
+                maybe_html.extend(
+                    ['!', '{']
+                        .into_iter()
+                        .map_while(|p| stream.allow_consume(p)),
+                );
 
-                        loop {
-                            if let Some(tt) = stream.allow_consume("else") {
-                                code.collect(tt);
+                match maybe_html.into_inner() {
+                    Ok(html) => {
+                        let branches = scope.map(Clone::clone);
 
-                                let else_if = stream.allow_consume("if");
-                                let stop = else_if.is_none();
-
-                                if let Some(tt) = else_if {
-                                    code.collect(tt);
-                                    code.extend(ShallowParser { stream });
-                                }
-
-                                if let TokenTree::Group(group) = stream.expect('{')? {
-                                    code.push(Code::Nested(Nested::parse(group, scope)?));
-                                }
-
-                                if stop {
-                                    break;
-                                }
-                            }
-                        }
-
-                        continue;
+                        code.push(Code::Html(Html::new(html.into_iter().collect(), branches)));
                     }
-                    None => (),
+                    Err(tokens) => code.extend(tokens),
                 }
 
-                code.collect(tt);
+                continue;
             }
-            TokenTree::Group(group) => {
-                code.push(Code::Nested(Nested::parse(group, scope)?));
-            }
-            tt => {
+            Token::If => {
+                let scope = Rc::new(Cell::new(0));
+
+                states.push(State(Token::Else, Mode::Eager, scope.clone()));
+                states.push(State(Token::Block, Mode::Lazy, scope));
+
                 code.collect(tt);
+
+                continue;
+            }
+            Token::Else => {
+                if let Some(State(_, _, scope)) = state {
+                    code.collect(tt);
+
+                    if let Some(tt) = stream.allow_consume("if") {
+                        code.collect(tt);
+
+                        states.push(State(Token::Else, Mode::Eager, scope.clone()));
+                        states.push(State(Token::Block, Mode::Lazy, scope));
+                    } else {
+                        states.push(State(Token::Block, Mode::Eager, scope));
+                    };
+
+                    continue;
+                }
+            }
+            Token::Block | Token::Group => {
+                let scope = state.as_ref().map(|State(_, _, ref scope)| scope).or(scope);
+
+                match tt {
+                    TokenTree::Group(group) => {
+                        code.push(Code::Nested(Nested::parse(group, scope)?))
+                    }
+                    _ => panic!("Token not matching TokenTree, this is a bug, please report it"),
+                }
+
+                continue;
             }
         }
+
+        code.collect(tt);
     }
 
     Ok(code.finish())
