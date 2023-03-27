8061947145f7f22f3be09e51dbaf7163d1a0bab9
Maciej Hirsz <hello@maciej.codes>
2023-03-27 17:11:07 +0100

    fmt

--- crates/kobold_qr/src/lib.rs
+++ crates/kobold_qr/src/lib.rs
@@ -2,8 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use kobold::prelude::*;
 use kobold::diff::fence_then;
+use kobold::prelude::*;
 use wasm_bindgen::prelude::*;
 
 use fast_qr::qr::QRBuilder;
