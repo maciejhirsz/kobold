ec767538d0e067f56796468bf0519944f39e43e2
Maciej Hirsz <hello@maciej.codes>
2023-03-27 12:40:46 +0100

    Add LICENSE Exhibit A notice to .rs files

--- crates/kobold/src/branching.rs
+++ crates/kobold/src/branching.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //! # Utilities for conditional rendering
 //!
 //! The [`view!`](crate::view) macro produces unique transient types, so you might run into compile errors when branching:
--- crates/kobold_macros/src/parse.rs
+++ crates/kobold_macros/src/parse.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //! [`ParseStream`](ParseStream), the [`Parse`](Parse) trait and utilities for working with
 //! token streams without `syn` or `quote`.
 
