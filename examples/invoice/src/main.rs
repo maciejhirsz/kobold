91448fa422cadbb72d77839e243c93962e76a24e
Luke Schoen <ltfschoen@gmail.com>
2023-04-05 09:30:22 +1000

    remove OptionalClass

--- examples/invoice/src/main.rs
+++ examples/invoice/src/main.rs
@@ -3,7 +3,6 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use kobold::prelude::*;
-use kobold::attribute::{OptionalClass};
 use kobold::branching::{Branch2, Branch3};
 use kobold::reexport::web_sys::HtmlTextAreaElement;
 use kobold_qr::KoboldQR;
