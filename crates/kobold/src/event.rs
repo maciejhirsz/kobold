39858b31b381dbb9e78439a072db9855b0e82ee3
Maciej Hirsz <1096222+maciejhirsz@users.noreply.github.com>
2023-04-15 17:58:04 +0100

    Don't store `JsValue` in `ListenerProduct` (#75)

--- crates/kobold/src/event.rs
+++ crates/kobold/src/event.rs
@@ -6,6 +6,7 @@
 
 use std::marker::PhantomData;
 use std::ops::Deref;
+use std::ptr::NonNull;
 
 use wasm_bindgen::closure::Closure;
 use wasm_bindgen::prelude::wasm_bindgen;
@@ -87,9 +88,11 @@ where
     E: EventCast,
     Self: Sized + 'static,
 {
-    fn build(self) -> ListenerProduct<Self>;
+    type Product: ListenerHandle;
 
-    fn update(self, p: &mut ListenerProduct<Self>);
+    fn build(self) -> Self::Product;
+
+    fn update(self, p: &mut Self::Product);
 }
 
 impl<E, F> Listener<E> for F
@@ -97,33 +100,45 @@ where
     F: FnMut(E) + 'static,
     E: EventCast,
 {
-    fn build(self) -> ListenerProduct<Self> {
-        let raw = Box::leak(Box::new(self));
+    type Product = ListenerProduct<Self, E>;
 
-        let js = Closure::wrap(unsafe {
-            Box::from_raw(raw as *mut dyn FnMut(E) as *mut dyn FnMut(web_sys::Event))
+    fn build(self) -> ListenerProduct<Self, E> {
+        ListenerProduct {
+            raw: Box::leak(Box::new(self)).into(),
+            _event: PhantomData,
+        }
+    }
+
+    fn update(self, p: &mut ListenerProduct<Self, E>) {
+        unsafe { *p.raw.as_ptr() = self };
+    }
+}
+
+pub struct ListenerProduct<F, E> {
+    raw: NonNull<F>,
+    _event: PhantomData<E>,
+}
+
+impl<F, E> Drop for ListenerProduct<F, E> {
+    fn drop(&mut self) {
+        unsafe { drop(Box::from_raw(self.raw.as_ptr())) }
+    }
+}
+
+pub trait ListenerHandle {
+    fn js(&self) -> JsValue;
+}
+
+impl<F, E> ListenerHandle for ListenerProduct<F, E>
+where
+    F: FnMut(E) + 'static,
+    E: EventCast,
+{
+    fn js(&self) -> JsValue {
+        Closure::wrap(unsafe {
+            Box::from_raw(self.raw.as_ptr() as *mut dyn FnMut(E) as *mut dyn FnMut(web_sys::Event))
         })
-        .into_js_value();
-
-        // `into_js_value` will _forget_ the previous Box, so we can safely reconstruct it
-        let boxed = unsafe { Box::from_raw(raw) };
-
-        ListenerProduct { js, boxed }
-    }
-
-    fn update(self, p: &mut ListenerProduct<Self>) {
-        *p.boxed = self;
-    }
-}
-
-pub struct ListenerProduct<F> {
-    js: JsValue,
-    boxed: Box<F>,
-}
-
-impl<F> ListenerProduct<F> {
-    pub fn js(&self) -> &JsValue {
-        &self.js
+        .into_js_value()
     }
 }
 
