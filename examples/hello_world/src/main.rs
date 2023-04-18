0ae654b62beddec864b6d55678fd4c897247fdc5
Maciej Hirsz <hello@maciej.codes>
2023-04-18 10:54:02 +0100

    Remove aria-foo from hello_world example

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -3,7 +3,7 @@ use kobold::prelude::*;
 #[component]
 fn Hello(name: &str) -> impl View + '_ {
     // No need to close tags at the end of the macro
-    view! { <h1 aria-foo="bar"> "Hello "{ name }"!" }
+    view! { <h1> "Hello "{ name }"!" }
 }
 
 fn main() {
