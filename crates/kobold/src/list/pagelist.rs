067136abe3028554d31837113391a8ff86b2cb60
Maciej Hirsz <hello@maciej.codes>
2023-04-20 12:30:23 +0100

    Working on builder for PageList

--- crates/kobold/src/list/pagelist.rs
+++ crates/kobold/src/list/pagelist.rs
@@ -1,8 +1,8 @@
-use std::marker::PhantomData;
+use std::alloc::{alloc, dealloc, Layout};
 use std::mem::MaybeUninit;
 use std::ops::{Deref, DerefMut};
+use std::pin::Pin;
 use std::ptr::NonNull;
-use std::alloc::{alloc, dealloc, Layout};
 
 use crate::internal::{In, Out};
 
@@ -26,17 +26,20 @@ union FatPtr<T> {
 
 impl<T> Page<T> {
     fn new(cap: usize) -> NonNull<Self> {
-        debug_assert_eq!(std::mem::size_of::<(NonNull<Head<T>>, usize)>(), std::mem::size_of::<NonNull<Page<T>>>());
+        // TODO: Use a dynamic MIN_PAGE_SIZE for T
+        let cap = std::cmp::max(cap, 1);
+
+        debug_assert_eq!(
+            std::mem::size_of::<(NonNull<Head<T>>, usize)>(),
+            std::mem::size_of::<NonNull<Page<T>>>()
+        );
 
         Vec::<u32>::new().into_boxed_slice();
 
         unsafe {
             let head = NonNull::new_unchecked(alloc(Self::layout(cap)) as *mut Head<T>);
 
-            head.as_ptr().write(Head {
-                next: None,
-                len: 0,
-            });
+            head.as_ptr().write(Head { next: None, len: 0 });
 
             FatPtr { raw: (head, cap) }.fat
         }
@@ -103,7 +106,7 @@ impl<T> Page<T> {
 
                 Some(slot)
             }
-            None => None
+            None => None,
         }
     }
 }
@@ -144,8 +147,7 @@ impl<T> PageList<T> {
     pub fn cursor(&mut self) -> Cursor<T> {
         Cursor {
             fold: 0,
-            page: self.first,
-            _pl: PhantomData,
+            page: Page::as_mut(self.first),
         }
     }
 }
@@ -169,50 +171,78 @@ impl<T> Drop for PageList<T> {
 
 struct Cursor<'a, T> {
     fold: usize,
-    page: NonNull<Page<T>>,
-    _pl: PhantomData<&'a mut PageList<T>>
+    page: &'a mut Page<T>,
 }
 
 impl<'a, T> Cursor<'a, T> {
     /// Drop all items and pages after current cursor position
     pub fn truncate_rest(&mut self) {
-        let page = Page::as_mut(self.page);
-
-        if let Some(to_drop) = page.data.get_mut(self.fold..page.len) {
+        if let Some(to_drop) = self.page.data.get_mut(self.fold..self.page.len) {
             for item in to_drop {
                 unsafe { item.assume_init_drop() };
-                page.len = self.fold;
+                self.page.len = self.fold;
             }
         }
 
-        if let Some(next) = page.next.take() {
+        if let Some(next) = self.page.next.take() {
             Page::dealloc(next);
         }
     }
 
-    // pub fn reserve(self, cap: usize) -> ReserveIter<'a, T> {
-    //     while let Some(next) = Page::as_mut(self.page).next {
-    //         self.page = next;
-    //     }
-    // }
+    pub fn reserve(mut self, cap: usize) -> Builder<'a, T> {
+        while let Some(next) = self.page.next.map(Page::as_mut) {
+            self.page = next;
+        }
+
+        if let Some(grow) = self.page.capacity().checked_sub(self.page.len + cap) {
+            self.page.next = Some(Page::new(grow));
+        }
+
+        Builder { page: self.page }
+    }
 }
 
 impl<'a, T> Iterator for Cursor<'a, T> {
     type Item = &'a mut T;
 
     fn next(&mut self) -> Option<&'a mut T> {
-        match Page::as_mut(self.page).get_mut(self.fold) {
-            Some(item) => {
-                self.fold += 1;
-                Some(item)
-            }
-            None => {
-                self.page = Page::as_mut(self.page).next?;
-                self.fold = 1;
+        loop {
+            if self.fold < self.page.len {
+                let item = unsafe { self.page.data.get_unchecked_mut(self.fold) };
 
-                Page::as_mut(self.page).get_mut(0)
+                self.fold += 1;
+
+                return Some(unsafe { &mut *(item.as_mut_ptr() as *mut T) });
             }
+
+            self.page = self.page.next.map(Page::as_mut)?;
+            self.fold = 0;
         }
+    }
+}
+
+struct Builder<'a, T> {
+    page: &'a mut Page<T>,
+}
+
+impl<T> Builder<'_, T> {
+    pub fn build<F>(&mut self, f: F)
+    where
+        F: FnOnce(In<T>) -> Out<T>,
+    {
+        let slot = loop {
+            match self.page.data.get_mut(self.page.len) {
+                Some(slot) => break slot,
+                None => {
+                    match self.page.next {
+                        Some(next) => self.page = Page::as_mut(next),
+                        None => return,
+                    }
+                },
+            }
+        };
+
+        unsafe { In::pinned(Pin::new_unchecked(slot), f) };
     }
 }
 
@@ -275,7 +305,10 @@ mod tests {
         assert_eq!(&Page::as_mut(list.first)[..], [42, 100]);
         assert_eq!(&Page::as_mut(list.last)[..], [404]);
 
-        assert_eq!(&list.cursor().map(|i| *i).collect::<Vec<_>>()[..], &[42, 100, 404]);
+        assert_eq!(
+            &list.cursor().map(|i| *i).collect::<Vec<_>>()[..],
+            &[42, 100, 404]
+        );
     }
 
     #[test]
