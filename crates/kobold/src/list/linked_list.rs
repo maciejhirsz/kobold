81569fe1dcf01b8c692cb77f1fa4afacc6040de1
Maciej Hirsz <hello@maciej.codes>
2023-04-21 13:03:32 +0100

    WIP truncate_rest code

--- crates/kobold/src/list/linked_list.rs
+++ crates/kobold/src/list/linked_list.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::mem::MaybeUninit;
 use std::pin::Pin;
 use std::ptr::{drop_in_place, NonNull};
@@ -79,6 +80,14 @@ impl<T> LinkedList<T> {
 
         LinkedList { len, first }
     }
+
+    pub fn cursor(&mut self) -> Cursor<T> {
+        Cursor {
+            cur: self.first,
+            idx: 0,
+            ll: self,
+        }
+    }
 }
 
 impl<T> Drop for LinkedList<T> {
@@ -102,6 +111,90 @@ impl<T> Drop for LinkedList<T> {
 
             Node::dealloc(node.into());
         }
+    }
+}
+
+pub struct Cursor<'cur, T> {
+    cur: NonNull<Node<T>>,
+    idx: usize,
+    ll: &'cur mut LinkedList<T>,
+}
+
+// impl<'cur, T> Cursor<'cur, T>
+// where
+//     T: 'cur,
+// {
+//     pub fn truncate_rest(self) {
+//         if self.idx == self.ll.len {
+//             return;
+//         }
+//         let len = self.ll.len;
+//         self.ll.len = self.idx;
+
+//         let node = Node::as_mut(self.cur);
+//         let local = self.idx % PAGE_SIZE;
+//         let mut remain = len - self.idx;
+
+//         let tail_list = if local == 0 {
+//             Some(self.cur)
+//         } else if let Some(slice) = remain.checked_sub(local) {
+//             Some(node.next)
+//         } else {
+//             None
+//         };
+
+//         let len = self.ll.len;
+//         self.ll.len = self.idx;
+
+//         // let node = Node::as_mut(self.cur);
+//         let mut remain = len - self.idx;
+
+//         match remain + local {
+//             0 => {
+//                 // do nothing
+//             }
+//             1..15 => {
+//                 // drop current
+//             }
+//         }
+
+//         unsafe {
+//             if self.idx != 0 && local == 0 {
+//                 //
+//             } else {
+
+//             }
+
+//             if local > 0 {
+//                 let to_drop = local..cmp::min(local + remain, PAGE_SIZE);
+//                 drop_in_place(&mut node.assume_page()[to_drop]);
+
+//                 remain -= local;
+//             }
+//         }
+//     }
+// }
+
+impl<'cur, T> Iterator for Cursor<'cur, T>
+where
+    T: 'cur,
+{
+    type Item = &'cur mut T;
+
+    fn next(&mut self) -> Option<&'cur mut T> {
+        if self.idx == self.ll.len {
+            return None;
+        }
+
+        let local = self.idx % PAGE_SIZE;
+
+        if self.idx != 0 && local == 0 {
+            self.cur = Node::as_mut(self.cur).next;
+        }
+
+        self.idx += 1;
+
+        Some(unsafe { Node::as_mut(self.cur).data[local].assume_init_mut() })
     }
 }
 
@@ -141,4 +234,29 @@ mod tests {
             assert_eq!(second.data[3].assume_init_mut(), "19");
         }
     }
+
+    #[test]
+    fn cursor_iter() {
+        let mut list = LinkedList::build(0..100, |n, p| p.put(n));
+
+        assert_eq!(list.len, 100);
+
+        for (left, right) in list.cursor().zip(0..100) {
+            assert_eq!(*left, right);
+        }
+    }
+
+    // #[test]
+    // fn cursor_truncate() {
+    //     let mut list = LinkedList::build(0..100, |n, p| p.put(n));
+
+    //     assert_eq!(list.len, 100);
+
+    //     let mut cur = list.cursor();
+
+    //     cur.by_ref().take(50).count();
+    //     cur.truncate_rest();
+
+    //     assert_eq!(list.len, 50);
+    // }
 }
