726be2bf2818119692b7dd2c3666c5a361e9ef3a
Maciej Hirsz <hello@maciej.codes>
2023-04-22 16:33:51 +0100

    Started refactoring linked_list_dyn to be like linked_list_opt

--- crates/kobold/src/list/linked_list_dyn.rs
+++ crates/kobold/src/list/linked_list_dyn.rs
@@ -9,7 +9,7 @@ use crate::internal::{In, Out};
 struct Node<T> {
     /// Pointer to the next `Node`. If this is a tail node,
     /// the address of this pointer will be uninitialized junk.
-    next: NonNull<Node<T>>,
+    next: Option<NonNull<Node<T>>>,
 
     /// All the elements of the `Node`
     data: [MaybeUninit<T>],
@@ -17,7 +17,7 @@ struct Node<T> {
 
 #[repr(C)]
 struct Head<T> {
-    next: NonNull<Node<T>>,
+    next: Option<NonNull<Node<T>>>,
 }
 
 union FatPtr<T> {
@@ -30,7 +30,7 @@ pub struct LinkedList<T> {
     len: usize,
 
     /// First `Node` in the list
-    first: NonNull<Node<T>>,
+    first: Option<NonNull<Node<T>>>,
 }
 
 impl<T> Node<T> {
@@ -55,19 +55,21 @@ impl<T> Node<T> {
         Vec::<u32>::new().into_boxed_slice();
 
         unsafe {
-            let head = NonNull::new_unchecked(alloc(Self::layout(cap)) as *mut Head<T>);
+            let head = alloc(Self::layout(cap)) as *mut Head<T>;
 
-            FatPtr { raw: (head, cap) }.fat
+            std::ptr::addr_of_mut!((*head).next).write(None);
+
+            FatPtr { raw: (NonNull::new_unchecked(head), cap) }.fat
         }
     }
 
-    fn dangling() -> NonNull<Self> {
-        unsafe {
-            let head = NonNull::dangling();
+    // fn dangling() -> NonNull<Self> {
+    //     unsafe {
+    //         let head = NonNull::dangling();
 
-            FatPtr { raw: (head, 0) }.fat
-        }
-    }
+    //         FatPtr { raw: (head, 0) }.fat
+    //     }
+    // }
 
     fn dealloc(ptr: NonNull<Self>) {
         unsafe { dealloc(ptr.as_ptr().cast(), Layout::for_value(ptr.as_ref())) }
@@ -111,64 +113,55 @@ impl<T> LinkedList<T> {
         I: IntoIterator<Item = U>,
         F: FnMut(U, In<T>) -> Out<T>,
     {
-        let mut first = Node::dangling();
+        let mut first = None;
 
         let mut iter = iter.into_iter();
         let mut next = &mut first;
         let mut len = 0;
+        let mut node_len = 0;
 
-        unsafe {
-            while let Some(item) = iter.next() {
-                *next = Node::new(iter.size_hint().0 + 1);
+        while let Some(item) = iter.next() {
+            let node = Node::as_mut(*next.get_or_insert_with(|| Node::new(iter.size_hint().0 + 1)));
 
-                let node = Node::as_mut(*next);
+            In::pinned(
+                unsafe { Pin::new_unchecked(&mut node.data[node_len]) },
+                |p| constructor(item, p),
+            );
 
-                In::pinned(Pin::new_unchecked(&mut node.data[0]), |p| {
-                    constructor(item, p)
-                });
+            node_len += 1;
 
-                len += 1;
-
-                for (slot, item) in node.data[1..].iter_mut().zip(iter.by_ref()) {
-                    In::pinned(Pin::new_unchecked(slot), |p| constructor(item, p));
-
-                    len += 1;
-                }
-
+            if node_len == node.capacity() {
+                len += node_len;
+                node_len = 0;
                 next = &mut node.next;
             }
         }
 
+        len += node_len;
+
         LinkedList { len, first }
     }
 
-    pub fn cursor(&mut self) -> Cursor<T> {
-        Cursor {
-            idx: 0,
-            cut: 0,
-            cur: &mut self.first,
-            len: &mut self.len,
-        }
-    }
+    // pub fn cursor(&mut self) -> Cursor<T> {
+    //     Cursor {
+    //         idx: 0,
+    //         cut: 0,
+    //         cur: &mut self.first,
+    //         len: &mut self.len,
+    //     }
+    // }
 }
 
 impl<T> Drop for LinkedList<T> {
     fn drop(&mut self) {
-        let mut node;
-
         unsafe {
-            while self.len > 0 {
-                node = Node::as_mut(self.first);
+            while let Some(node) = self.first {
+                let node = Node::as_mut(node);
 
-                if self.len > node.capacity() {
-                    drop_in_place(node.assume_slice(node.capacity()));
+                drop_in_place(node.assume_slice(std::cmp::min(self.len, node.capacity())));
 
-                    self.first = node.next;
-                    self.len -= node.capacity();
-                } else {
-                    drop_in_place(node.assume_slice(self.len));
-                    self.len = 0;
-                }
+                self.first = node.next;
+                self.len = self.len.wrapping_sub(node.capacity());
 
                 Node::dealloc(node.into());
             }
@@ -176,108 +169,108 @@ impl<T> Drop for LinkedList<T> {
     }
 }
 
-pub struct Cursor<'cur, T> {
-    idx: usize,
-    cut: usize,
-    cur: &'cur mut NonNull<Node<T>>,
-    len: &'cur mut usize,
-}
+// pub struct Cursor<'cur, T> {
+//     idx: usize,
+//     cut: usize,
+//     cur: &'cur mut NonNull<Node<T>>,
+//     len: &'cur mut usize,
+// }
 
-impl<'cur, T> Cursor<'cur, T>
-where
-    T: 'cur,
-{
-    // pub fn truncate_rest(self) -> Tail<'cur, T> {
-    //     if self.idx == *self.len {
-    //         return Tail {
-    //             cur: self.cur,
-    //             len: self.len,
-    //         };
-    //     }
+// impl<'cur, T> Cursor<'cur, T>
+// where
+//     T: 'cur,
+// {
+//     pub fn truncate_rest(self) -> Tail<'cur, T> {
+//         if self.idx == *self.len {
+//             return Tail {
+//                 cur: self.cur,
+//                 len: self.len,
+//             };
+//         }
 
-    //     let mut cur = *self.cur;
-    //     let mut remain = *self.len - self.idx;
-    //     let local = self.idx % PAGE_SIZE;
+//         let mut cur = *self.cur;
+//         let mut remain = *self.len - self.idx;
+//         let local = self.idx % PAGE_SIZE;
 
-    //     if local != 0 {
-    //         let node = cur;
-    //         let mut drop_local = PAGE_SIZE - local;
+//         if local != 0 {
+//             let node = cur;
+//             let mut drop_local = PAGE_SIZE - local;
 
-    //         if drop_local <= remain {
-    //             cur = Node::as_mut(cur).next;
-    //             remain -= drop_local;
-    //         } else {
-    //             drop_local = remain;
-    //             remain = 0;
-    //         };
+//             if drop_local <= remain {
+//                 cur = Node::as_mut(cur).next;
+//                 remain -= drop_local;
+//             } else {
+//                 drop_local = remain;
+//                 remain = 0;
+//             };
 
-    //         unsafe {
-    //             drop_in_place(&mut Node::as_mut(node).assume_page()[local..local + drop_local]);
-    //         }
-    //     };
+//             unsafe {
+//                 drop_in_place(&mut Node::as_mut(node).assume_page()[local..local + drop_local]);
+//             }
+//         };
 
-    //     *self.len = self.idx;
+//         *self.len = self.idx;
 
-    //     drop(LinkedList {
-    //         len: remain,
-    //         first: cur,
-    //     });
+//         drop(LinkedList {
+//             len: remain,
+//             first: cur,
+//         });
 
-    //     Tail {
-    //         cur: self.cur,
-    //         len: self.len,
-    //     }
-    // }
+//         Tail {
+//             cur: self.cur,
+//             len: self.len,
+//         }
+//     }
 
-    pub fn has_next(&self) -> bool {
-        self.idx != *self.len
-    }
+//     pub fn has_next(&self) -> bool {
+//         self.idx != *self.len
+//     }
 
-    pub fn pair<I, F, U>(&mut self, iter: I, mut each: F)
-    where
-        I: IntoIterator<Item = U>,
-        F: FnMut(&mut T, U),
-    {
-        let mut iter = iter.into_iter();
+//     pub fn pair<I, F, U>(&mut self, iter: I, mut each: F)
+//     where
+//         I: IntoIterator<Item = U>,
+//         F: FnMut(&mut T, U),
+//     {
+//         let mut iter = iter.into_iter();
 
-        while self.has_next() {
-            if let Some(item) = iter.next() {
-                each(self.next().unwrap(), item);
-                continue;
-            }
+//         while self.has_next() {
+//             if let Some(item) = iter.next() {
+//                 each(self.next().unwrap(), item);
+//                 continue;
+//             }
 
-            break;
-        }
-    }
-}
+//             break;
+//         }
+//     }
+// }
 
-impl<'cur, T> Iterator for Cursor<'cur, T>
-where
-    T: 'cur,
-{
-    type Item = &'cur mut T;
+// impl<'cur, T> Iterator for Cursor<'cur, T>
+// where
+//     T: 'cur,
+// {
+//     type Item = &'cur mut T;
 
-    fn next(&mut self) -> Option<&'cur mut T> {
-        let cur = if self.idx == *self.len {
-            return None;
-        } else {
-            Node::as_mut(*self.cur)
-        };
+//     fn next(&mut self) -> Option<&'cur mut T> {
+//         let cur = if self.idx == *self.len {
+//             return None;
+//         } else {
+//             Node::as_mut(*self.cur)
+//         };
 
-        let cap = cur.data.len();
-        let item = unsafe { cur.data[self.idx].assume_init_mut() };
+//         let cap = cur.data.len();
+//         let item = unsafe { cur.data[self.idx].assume_init_mut() };
 
-        self.idx += 1;
+//         self.idx += 1;
 
-        if self.idx == cap {
-            self.idx = 0;
-            self.cut += cap;
-            self.cur = &mut cur.next;
-        }
+//         if self.idx == cap {
+//             self.idx = 0;
+//             self.cut += cap;
+//             self.cur = &mut cur.next;
+//         }
 
-        Some(item)
-    }
-}
+//         Some(item)
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -296,7 +289,7 @@ mod tests {
 
         assert_eq!(list.len, 128);
 
-        let first = Node::as_mut(list.first);
+        let first = Node::as_mut(list.first.unwrap());
 
         unsafe {
             assert_eq!(first.data[0].assume_init_read(), 0);
@@ -309,21 +302,21 @@ mod tests {
         let list = LinkedList::build(0..20, |n, p| p.put(Box::new(n)));
 
         unsafe {
-            let first = Node::as_mut(list.first);
+            let first = Node::as_mut(list.first.unwrap());
 
             assert_eq!(**first.data[0].assume_init_ref(), 0);
             assert_eq!(**first.data[19].assume_init_ref(), 19);
         }
     }
 
-    #[test]
-    fn cursor_iter() {
-        let mut list = LinkedList::build(0..100, |n, p| p.put(n));
+    // #[test]
+    // fn cursor_iter() {
+    //     let mut list = LinkedList::build(0..100, |n, p| p.put(n));
 
-        assert_eq!(list.len, 100);
+    //     assert_eq!(list.len, 100);
 
-        for (left, right) in list.cursor().zip(0..100) {
-            assert_eq!(*left, right);
-        }
-    }
+    //     for (left, right) in list.cursor().zip(0..100) {
+    //         assert_eq!(*left, right);
+    //     }
+    // }
 }
