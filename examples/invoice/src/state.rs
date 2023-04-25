56fd15d0f8bafe675d87d0a1dd7ac1086682d23f
Luke Schoen <ltfschoen@gmail.com>
2023-04-25 23:17:59 +1000

    update details table. causes memory access out of bounds. edits dont update local storage

--- examples/invoice/src/state.rs
+++ examples/invoice/src/state.rs
@@ -4,10 +4,11 @@
 
 use std::str::FromStr;
 use gloo_storage::{LocalStorage, Storage};
-use wasm_bindgen::UnwrapThrowExt;
+use gloo_utils::format::JsValueSerdeExt;
+use wasm_bindgen::{JsValue, UnwrapThrowExt};
 // use wasm_bindgen::prelude::wasm_bindgen;
 use serde::{Serialize, Deserialize};
-use serde_json::to_string;
+use serde_json::{from_str, to_string};
 use log::{info, debug, error, warn};
 use std::convert::TryInto;
 
@@ -79,11 +80,6 @@ impl Default for State {
     }
 }
 
-fn convert_vec_to_arr<T, const N: usize>(v: Vec<T>) -> [T; N] {
-    v.try_into()
-        .unwrap_or_else(|v: Vec<T>| panic!("Expected a Vec of length {} but it was {}", N, v.len()))
-}
-
 impl State {
     pub fn mock() -> Self {
         State {
@@ -101,26 +97,93 @@ impl State {
     }
 
     #[inline(never)]
+    // store the updated state in web browser local storage
     pub fn store(&self) {
         let main_str = to_string(&self.main.table).unwrap();
         let details_str = to_string(&self.details.table).unwrap();
-
         debug!("updating store: {:?}\n\n{:?}", main_str.as_str(), details_str.as_str());
-
         LocalStorage::raw().set_item(KEY_MAIN, main_str.as_str()).ok();
         LocalStorage::raw().set_item(KEY_DETAILS, details_str.as_str()).ok();
     }
 
+    // store in state edits by user to the 'main' table of the UI
     pub fn update_main(&mut self, row: usize, col: usize, value: String) {
         let old_storage = self.main.table.source.get_text(&self.main.table.rows[row][col]);
         if value != old_storage {
             self.store();
         }
+
+        // TODO - update this similar to how i've updated `update_details` method
     }
 
+    // store in state edits by user to the 'details' table of the UI
     pub fn update_details(&mut self, row: usize, col: usize, value: String) {
+        debug!("update_details: {:?}\n{:?}\n{:?}", row, col, value);
         let old_storage = self.details.table.source.get_text(&self.details.table.rows[row][col]);
+        debug!("update_details old storage: {:?}", old_storage);
         if value != old_storage {
+            debug!("updating details since different");
+            self.details.table.rows[row][col] = Text::Owned(value.into());
+            // TODO - convert old_storage to an object variable
+
+            let serialized = serde_json::to_string(&self.details.table).unwrap();
+            let value = JsValue::from_serde(&serialized).unwrap();
+            debug!("details table {:#?}", value);
+            let details_table: Table = serde_json::from_str(&serialized).unwrap();
+            debug!("details table source {:#?}", &details_table.source.source);
+            let data: &str = &details_table.source.source.to_string();
+
+
+            let count_newlines = data.matches("\n").count();
+            let (row1_idx_end, _) = data.match_indices("\n").nth(0).unwrap();
+            let row0_idx_start = row1_idx_end + 1; // where +1 is to skip the `\n`
+            let (row0_idx_end, _) = data.match_indices("\n").nth(1).unwrap();
+            let row2_idx_start = row0_idx_end + 1;
+            let row2_idx_end = data.len() - count_newlines;
+            debug!("row1_idx_end {:#?}", row1_idx_end); // variables
+            debug!("row0_idx_end {:#?}", row0_idx_end); // labels
+            debug!("row2_idx_end {:#?}", row2_idx_end); // data
+
+            let mut row0_vec: Vec<String> = Vec::new();
+            let row0 = &data[row0_idx_start..row0_idx_end];
+            row0_vec = row0.split(",").map(|x| x.to_string()).collect();
+            debug!("row0_vec {:#?}", row0_vec);
+
+            let mut row2_vec: Vec<String> = Vec::new();
+            let row2 = &data[row2_idx_start..row2_idx_end];
+            row2_vec = row2.split(",").map(|x| x.to_string()).collect();
+            debug!("row2_vec {:#?}", row2_vec);
+
+            // let blank_str = "".to_string();
+            if row == 0 {
+                debug!("updating row0_vec[col] {:#?}", row0_vec[col]);
+                // https://docs.rs/wasm-bindgen/0.2.84/wasm_bindgen/struct.JsValue.html#method.as_string
+                row0_vec[col] = match value.as_string() {
+                    Some(v) => v.to_string(),
+                    None => "".to_string(),
+                };
+            } else if row == 1 { // need row variable to be 1 to update row data 2
+                debug!("updating row2_vec[col] {:#?}", row2_vec[col]);
+                row2_vec[col] = match value.as_string() {
+                    Some(v) => v.to_string(),
+                    None => "".to_string(),
+                };
+            } else {
+                panic!("cannot update this row from the ui"); // `row` value of `2` won't be provided to this fn
+            }
+
+            // check that the table source at `rows[row][col]` has been updated with the new value
+            let updated_old_storage = self.details.table.source.get_text(&self.details.table.rows[row][col]);
+            debug!("update_details updated_old_storage: {:?}", updated_old_storage);
+
+            // TODO - whilst it is being updated in the above logs, when we later call `self.store()`
+            // the value of the data that we retrieve from `self` in that method is different when we run
+            // `to_string(&self.details.table).unwrap()`, so changes to `self` in this method aren't being
+            // reflected in the `self.store()` method
+
+            // TODO - we're already doing this in main.rs onkeydown so maybe we don't need it here too
+            self.editing_details = Editing::None;
+
             self.store();
         }
     }
