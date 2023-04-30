d3902506e7e1a9557d94c7a3c75fa5beb0b3690b
Luke Schoen <ltfschoen@gmail.com>
2023-05-01 08:25:03 +1000

    restore Editor

--- examples/invoice/src/main.rs
+++ examples/invoice/src/main.rs
@@ -21,185 +21,185 @@ use components::{
 
 fn main() {
 
-    // #[component]
-    // pub fn Editor() -> impl View {
-    //     stateful(State::default, |state| {
-    //         debug!("Editor()");
+    #[component]
+    pub fn Editor() -> impl View {
+        stateful(State::default, |state| {
+            debug!("Editor()");
     
-    //         let onload_details = state.bind_async(|state, event: Event<InputElement>| async move {
-    //             debug!("onload_details");
-    //             let file = match event.target().files().and_then(|list| list.get(0)) {
-    //                 Some(file) => file,
-    //                 None => return,
-    //             };
+            let onload_details = state.bind_async(|state, event: Event<InputElement>| async move {
+                debug!("onload_details");
+                let file = match event.target().files().and_then(|list| list.get(0)) {
+                    Some(file) => file,
+                    None => return,
+                };
     
-    //             state.update(|state| state.details.filename = file.name());
+                state.update(|state| state.details.filename = file.name());
     
-    //             if let Ok(table) = csv::read_file(file).await {
-    //                 debug!("state.details.table {:#?}", table);
-    //                 // https://docs.rs/kobold/latest/kobold/stateful/struct.Signal.html#method.update
-    //                 state.update(move |state| {
-    //                     state.details.table = table;
-    //                     state.store(); // update local storage
-    //                 });
-    //             }
-    //         });
+                if let Ok(table) = csv::read_file(file).await {
+                    debug!("state.details.table {:#?}", table);
+                    // https://docs.rs/kobold/latest/kobold/stateful/struct.Signal.html#method.update
+                    state.update(move |state| {
+                        state.details.table = table;
+                        state.store(); // update local storage
+                    });
+                }
+            });
     
-    //         let onsave_details = state.bind_async(|state, event: MouseEvent<HtmlElement>| async move {
-    //             // closure required just to debug with access to state fields, since otherwise it'd trigger a render
-    //             state.update_silent(|state| {
-    //                 debug!("onsave_details: {:?}", &state.details);
-    //             });
+            let onsave_details = state.bind_async(|state, event: MouseEvent<HtmlElement>| async move {
+                // closure required just to debug with access to state fields, since otherwise it'd trigger a render
+                state.update_silent(|state| {
+                    debug!("onsave_details: {:?}", &state.details);
+                });
     
-    //             state.update(|state| {
-    //                 // update local storage and state so that &state.details isn't
-    //                 // `Content { filename: "\0\0\0\0\0\0\0", table: Table { source: TextSource
-    //                 //   { source: "\0" }, columns: [Insitu(0..0)], rows: [] } }`
-    //                 state.store();
-    //                 match csv::generate_csv_data_for_download(&state.details) {
-    //                     Ok(csv_data) => {
-    //                         debug!("csv_data {:?}", csv_data);
-    //                         // cast String into a byte slice
-    //                         let csv_data_byte_slice: &[u8] = csv_data.as_bytes();
-    //                         js::browser_js::run_save_file(&state.details.filename, csv_data_byte_slice);
-    //                     }
-    //                     Err(err) => {
-    //                         panic!(
-    //                             "failed to generate csv data for download {:?}",
-    //                             state.details.filename
-    //                         );
-    //                     }
-    //                 };
-    //                 debug!(
-    //                     "successfully generate csv data for download {:?}",
-    //                     state.details.filename
-    //                 );
-    //             });
-    //         });
+                state.update(|state| {
+                    // update local storage and state so that &state.details isn't
+                    // `Content { filename: "\0\0\0\0\0\0\0", table: Table { source: TextSource
+                    //   { source: "\0" }, columns: [Insitu(0..0)], rows: [] } }`
+                    state.store();
+                    match csv::generate_csv_data_for_download(&state.details) {
+                        Ok(csv_data) => {
+                            debug!("csv_data {:?}", csv_data);
+                            // cast String into a byte slice
+                            let csv_data_byte_slice: &[u8] = csv_data.as_bytes();
+                            js::browser_js::run_save_file(&state.details.filename, csv_data_byte_slice);
+                        }
+                        Err(err) => {
+                            panic!(
+                                "failed to generate csv data for download {:?}",
+                                state.details.filename
+                            );
+                        }
+                    };
+                    debug!(
+                        "successfully generate csv data for download {:?}",
+                        state.details.filename
+                    );
+                });
+            });
     
-    //         let onload_main = state.bind_async(|state, event: Event<InputElement>| async move {
-    //             let file = match event.target().files().and_then(|list| list.get(0)) {
-    //                 Some(file) => file,
-    //                 None => return,
-    //             };
+            let onload_main = state.bind_async(|state, event: Event<InputElement>| async move {
+                let file = match event.target().files().and_then(|list| list.get(0)) {
+                    Some(file) => file,
+                    None => return,
+                };
     
-    //             state.update(|state| state.main.filename = file.name());
+                state.update(|state| state.main.filename = file.name());
     
-    //             if let Ok(table) = csv::read_file(file).await {
-    //                 state.update(move |state| {
-    //                     state.main.table = table;
-    //                     state.store(); // update local storage
-    //                 });
-    //             }
-    //         });
+                if let Ok(table) = csv::read_file(file).await {
+                    state.update(move |state| {
+                        state.main.table = table;
+                        state.store(); // update local storage
+                    });
+                }
+            });
     
-    //         view! {
-    //             <div .invoice-wrapper>
-    //                 <section .invoiceapp>
-    //                     <header .header>
-    //                         <h1>"Invoice"</h1>
-    //                     </header>
-    //                     <section .main>
-    //                         <div class="container">
-    //                             <input type="file" id="file-input" accept="text/csv" onchange={onload_details} />
-    //                             <input type="button" onclick="document.getElementById('file-input').click()" value="Upload CSV file" />
-    //                             <label for="file-input" class="label">{ ref state.details.filename }</label>
-    //                             <button #button-file-save type="button" onclick={onsave_details}>"Save to CSV file"</button>
-    //                             <br />
-    //                         </div>
-    //                         <table
-    //                             onkeydown={
-    //                                 state.bind(move |state, event: KeyboardEvent<_>| {
-    //                                     if matches!(event.key().as_str(), "Esc" | "Escape") {
-    //                                         state.editing_details = Editing::None;
+            view! {
+                <div .invoice-wrapper>
+                    <section .invoiceapp>
+                        <header .header>
+                            <h1>"Invoice"</h1>
+                        </header>
+                        <section .main>
+                            <div class="container">
+                                <input type="file" id="file-input" accept="text/csv" onchange={onload_details} />
+                                <input type="button" onclick="document.getElementById('file-input').click()" value="Upload CSV file" />
+                                <label for="file-input" class="label">{ ref state.details.filename }</label>
+                                <button #button-file-save type="button" onclick={onsave_details}>"Save to CSV file"</button>
+                                <br />
+                            </div>
+                            <table
+                                onkeydown={
+                                    state.bind(move |state, event: KeyboardEvent<_>| {
+                                        if matches!(event.key().as_str(), "Esc" | "Escape") {
+                                            state.editing_details = Editing::None;
     
-    //                                         Then::Render
-    //                                     } else {
-    //                                         Then::Stop
-    //                                     }
-    //                                 })
-    //                             }
-    //                         >
-    //                             <thead.details>
-    //                                 <tr>
-    //                                 {
-    //                                     for state.details.table.columns().map(|col| view! {
-    //                                         <HeadDetails {col} row={1} {state} />
-    //                                     })
-    //                                 }
-    //                                 </tr>
-    //                             </thead>
-    //                             <tbody.details>
-    //                                 <tr>
-    //                                 {
-    //                                     for state.details.table.columns().map(|col| view! {
-    //                                         <CellDetails {col} row={0} {state} />
-    //                                     })
-    //                                 }
-    //                                 </tr>
-    //                             </tbody>
-    //                         </table>
-    //                         <div #input-file-select>
-    //                             <h1>{ ref state.main.filename }</h1>
-    //                             <input type="file" accept="text/csv" onchange={onload_main} />
-    //                         </div>
-    //                         <table
-    //                             onkeydown={
-    //                                 state.bind(move |state, event: KeyboardEvent<_>| {
-    //                                     if matches!(event.key().as_str(), "Esc" | "Escape") {
-    //                                         state.editing_main = Editing::None;
+                                            Then::Render
+                                        } else {
+                                            Then::Stop
+                                        }
+                                    })
+                                }
+                            >
+                                <thead.details>
+                                    <tr>
+                                    {
+                                        for state.details.table.columns().map(|col| view! {
+                                            <HeadDetails {col} row={1} {state} />
+                                        })
+                                    }
+                                    </tr>
+                                </thead>
+                                <tbody.details>
+                                    <tr>
+                                    {
+                                        for state.details.table.columns().map(|col| view! {
+                                            <CellDetails {col} row={0} {state} />
+                                        })
+                                    }
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div #input-file-select>
+                                <h1>{ ref state.main.filename }</h1>
+                                <input type="file" accept="text/csv" onchange={onload_main} />
+                            </div>
+                            <table
+                                onkeydown={
+                                    state.bind(move |state, event: KeyboardEvent<_>| {
+                                        if matches!(event.key().as_str(), "Esc" | "Escape") {
+                                            state.editing_main = Editing::None;
     
-    //                                         Then::Render
-    //                                     } else {
-    //                                         Then::Stop
-    //                                     }
-    //                                 })
-    //                             }
-    //                         >
-    //                             <thead>
-    //                                 <tr>
-    //                                 {
-    //                                     for state.main.table.columns().map(|col| view! {
-    //                                         <Head {col} row={0} {state} />
-    //                                     })
-    //                                 }
-    //                                 </tr>
-    //                             </thead>
-    //                             <tbody>
-    //                             {
-    //                                 for state.main.table.rows().map(move |row| view! {
-    //                                     <tr>
-    //                                     {
-    //                                         for state.main.table.columns().map(move |col| view! {
-    //                                             <Cell {col} {row} {state} />
-    //                                         })
-    //                                     }
-    //                                     </tr>
-    //                                 })
-    //                             }
-    //                             </tbody>
-    //                         </table>
-    //                     </section>
-    //                 </section>
-    //                 <footer .info>
-    //                 {
-    //                     (
-    //                         state.editing_main == Editing::None &&
-    //                         state.editing_details == Editing::None
-    //                     ).then(|| view! {
-    //                         <p>"Hint: Double-click to edit an invoice field"</p>
-    //                     })
-    //                 }
-    //                 </footer>
-    //             </div>
-    //         }
-    //     })
-    // }
+                                            Then::Render
+                                        } else {
+                                            Then::Stop
+                                        }
+                                    })
+                                }
+                            >
+                                <thead>
+                                    <tr>
+                                    {
+                                        for state.main.table.columns().map(|col| view! {
+                                            <Head {col} row={0} {state} />
+                                        })
+                                    }
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                {
+                                    for state.main.table.rows().map(move |row| view! {
+                                        <tr>
+                                        {
+                                            for state.main.table.columns().map(move |col| view! {
+                                                <Cell {col} {row} {state} />
+                                            })
+                                        }
+                                        </tr>
+                                    })
+                                }
+                                </tbody>
+                            </table>
+                        </section>
+                    </section>
+                    <footer .info>
+                    {
+                        (
+                            state.editing_main == Editing::None &&
+                            state.editing_details == Editing::None
+                        ).then(|| view! {
+                            <p>"Hint: Double-click to edit an invoice field"</p>
+                        })
+                    }
+                    </footer>
+                </div>
+            }
+        })
+    }
     
-    // // Demonstrate use of Rust `wasm-bindgen` https://rustwasm.github.io/docs/wasm-bindgen
-    // js::browser_js::run();
-    // wasm_logger::init(wasm_logger::Config::default());
-    // kobold::start(view! {
-    //     <Editor />
-    // });
+    // Demonstrate use of Rust `wasm-bindgen` https://rustwasm.github.io/docs/wasm-bindgen
+    js::browser_js::run();
+    wasm_logger::init(wasm_logger::Config::default());
+    kobold::start(view! {
+        <Editor />
+    });
 }
