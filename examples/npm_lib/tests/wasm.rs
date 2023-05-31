12c5db27f38fbc0148ff774b17fd827e96812e65
Luke Schoen <ltfschoen@gmail.com>
2023-05-31 10:28:09 +1000

    remove useless onclick_pjs_process_async

--- examples/npm_lib/tests/wasm.rs
+++ examples/npm_lib/tests/wasm.rs
@@ -7,13 +7,9 @@ use super::*;
 
 use crate::js::browser_js::run_npm_lib;
 
-async fn onclick_pjs_process_async() -> Result<wasm_bindgen::JsValue, wasm_bindgen::JsValue> {
-    run_npm_lib().await
-}
-
 #[wasm_bindgen_test]
 async fn test_onclick_pjs_process() {
-    let expected = "0x00";
+    let expected = "0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3";
 
     let actual = run_npm_lib().await.unwrap().as_string().unwrap();
 
