7edd9c169401ce7e7e3269cf08e279ad83ed76be
Maciej Hirsz <hello@maciej.codes>
2024-04-11 15:41:47 +0100

    Documented new keyword

--- crates/kobold/src/keywords.rs
+++ crates/kobold/src/keywords.rs
@@ -5,7 +5,7 @@
 //! Keyword handles for `{ ... }` expressions in the [`view!`](crate::view) macro.
 
 use crate::diff::{Eager, Ref, Static};
-use crate::list;
+use crate::list::{Bounded, List};
 use crate::View;
 
 /// `{ for ... }`: turn an [`IntoIterator`] type into a [`View`].
@@ -15,17 +15,42 @@ use crate::View;
 /// view! {
 ///     <h1>"Integers 1 to 10:"</h1>
 ///     <ul>
-///     { for (1..=10).map(|n| view! { <li>{ n }</li> }) }
+///     { for (1..=10).map(|n| view! { <li>{ n } }) }
 ///     </ul>
 /// }
 /// # ;
 /// ```
-pub const fn r#for<T>(iterator: T) -> list::List<T>
+pub const fn r#for<T>(iterator: T) -> List<T>
 where
     T: IntoIterator,
     T::Item: View,
 {
-    list::List(iterator)
+    List::new(iterator)
+}
+
+/// `{ for<N> ... }`: turn an [`IntoIterator`] type into a [`View`],
+/// bounded to max length of `N`.
+///
+/// This should be used only for small values of `N`.
+///
+/// # Performance
+///
+/// The main advantage in using `for<N>` over regular `for` is that the
+/// bounded variant of a [`List`] doesn't need to allocate as the max size is fixed
+/// and known at compile time.
+///
+/// ```
+/// # use kobold::prelude::*;
+/// view! {
+///     <h1>"Integers 1 to 10:"</h1>
+///     <ul>
+///     { for<10> (1..=10).map(|n| view! { <li>{ n } }) }
+///     </ul>
+/// }
+/// # ;
+/// ```
+pub const fn for_bounded<T, const N: usize>(iterator: T) -> List<T, Bounded<N>> {
+    List::new_bounded(iterator)
 }
 
 /// `{ ref ... }`: diff this value by its reference address.
--- crates/kobold/src/list.rs
+++ crates/kobold/src/list.rs
@@ -4,17 +4,39 @@
 
 //! Utilities for rendering lists
 
+use std::marker::PhantomData;
+
 use crate::internal::{In, Out};
 use crate::View;
 
+pub mod bounded;
 pub mod unbounded;
 
+use bounded::BoundedProduct;
 use unbounded::ListProduct;
+
+/// Zero-sized marker making the [`List`] unbounded: it can grow to arbitrary
+/// size but will require memory allocation.
+pub struct Unbounded;
+
+/// Zero-sized marker making the [`List`] bounded to a max length of `N`:
+/// elements over the limit are ignored and no allocations are made.
+pub struct Bounded<const N: usize>;
 
 /// Wrapper type that implements `View` for iterators, created by the
 /// [`for`](crate::keywords::for) keyword.
 #[repr(transparent)]
-pub struct List<T>(pub(crate) T);
+pub struct List<T, B = Unbounded>(T, PhantomData<B>);
+
+impl<T> List<T> {
+    pub const fn new(item: T) -> Self {
+        List(item, PhantomData)
+    }
+
+    pub const fn new_bounded<const N: usize>(item: T) -> List<T, Bounded<N>> {
+        List(item, PhantomData)
+    }
+}
 
 impl<T> View for List<T>
 where
@@ -32,16 +54,31 @@ where
     }
 }
 
+impl<T, const N: usize> View for List<T, Bounded<N>>
+where
+    T: IntoIterator,
+    <T as IntoIterator>::Item: View,
+{
+    type Product = BoundedProduct<<T::Item as View>::Product, N>;
+
+    fn build(self, p: In<Self::Product>) -> Out<Self::Product> {
+        BoundedProduct::build(self.0.into_iter(), p)
+    }
+
+    fn update(self, p: &mut Self::Product) {
+        p.update(self.0.into_iter());
+    }
+}
 
 impl<V: View> View for Vec<V> {
     type Product = ListProduct<V::Product>;
 
     fn build(self, p: In<Self::Product>) -> Out<Self::Product> {
-        List(self).build(p)
+        List::new(self).build(p)
     }
 
     fn update(self, p: &mut Self::Product) {
-        List(self).update(p);
+        List::new(self).update(p);
     }
 }
 
@@ -52,11 +89,11 @@ where
     type Product = ListProduct<<&'a V as View>::Product>;
 
     fn build(self, p: In<Self::Product>) -> Out<Self::Product> {
-        List(self).build(p)
+        List::new(self).build(p)
     }
 
     fn update(self, p: &mut Self::Product) {
-        List(self).update(p)
+        List::new(self).update(p)
     }
 }
 
@@ -64,10 +101,10 @@ impl<V: View, const N: usize> View for [V; N] {
     type Product = ListProduct<V::Product>;
 
     fn build(self, p: In<Self::Product>) -> Out<Self::Product> {
-        List(self).build(p)
+        List::new(self).build(p)
     }
 
     fn update(self, p: &mut Self::Product) {
-        List(self).update(p)
+        List::new(self).update(p)
     }
 }
