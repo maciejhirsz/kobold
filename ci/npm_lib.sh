9eb7c6150127147fd6c15ac3ae714ee687195ea9
Luke Schoen <ltfschoen@gmail.com>
2025-03-10 16:56:04 -0400

    use rust not miri

--- ci/npm_lib.sh
+++ ci/npm_lib.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
 set -e
 
-MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-unknown-linux-gnu/miri)
-echo "Installing latest nightly with Miri: $MIRI_NIGHTLY"
-rustup set profile minimal
-rustup default "$MIRI_NIGHTLY"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+rustup toolchain install nightly
+rustup default stable
+rustup update
+rustup update nightly
+rustup target add wasm32-unknown-unknown
+rustup target add wasm32-unknown-unknown --toolchain nightly
 
-rustup component add miri
-cargo miri setup
+rustup set profile minimal
+
+# MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-unknown-linux-gnu/miri)
+# echo "Installing latest nightly with Miri: $MIRI_NIGHTLY"
+# rustup set profile minimal
+# rustup default "$MIRI_NIGHTLY"
+
+# rustup component add miri
+# cargo miri setup
 
 cd examples/npm_lib
 
-npm install
+# npm install
+npm install -g yarn
+yarn
 yarn run esbuild
 # rustup update
 # PATH=$HOME/.cargo/bin:$PATH
@@ -19,4 +32,5 @@ rustup target add wasm32-unknown-unknown --toolchain nightly
 # Note: The `--vers "0.2.100"` must match the version of `wasm-bindgen` in Cargo.toml file
 cargo install wasm-bindgen-cli --vers "0.2.100"
 
-MIRIFLAGS='-Zmiri-strict-provenance' cargo +nightly miri test --package kobold_npm_lib_example --bin main --target wasm32-unknown-unknown -Zdoctest-xcompile --verbose
+# MIRIFLAGS='-Zmiri-strict-provenance' cargo +nightly miri test --package kobold_npm_lib_example --bin main --target wasm32-unknown-unknown -Zdoctest-xcompile --verbose
+cargo +nightly test --package kobold_npm_lib_example --bin main --target wasm32-unknown-unknown -Zdoctest-xcompile --verbose
