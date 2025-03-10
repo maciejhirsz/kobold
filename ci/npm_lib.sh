79564191fa2545b035ca83fbd258b8027079cc32
Luke Schoen <ltfschoen@gmail.com>
2025-03-10 16:08:23 -0400

    update ci with rustup target add wasm32-unknown-unknown

--- ci/npm_lib.sh
+++ ci/npm_lib.sh
@@ -2,4 +2,8 @@
 set -e
 
 cd examples/npm_lib
+rustup update
+rustup target add wasm32-unknown-unknown
+# Note: The `--vers "0.2.100"` must match the version of `wasm-bindgen` in Cargo.toml file
+cargo install wasm-bindgen-cli --vers "0.2.100"
 cargo +nightly test --package kobold_npm_lib_example --bin main --target wasm32-unknown-unknown -Zdoctest-xcompile --verbose
