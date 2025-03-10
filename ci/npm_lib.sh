1a958d2543d2970e19cb22b8b4264eec4661f937
Luke Schoen <ltfschoen@gmail.com>
2025-03-10 16:58:21 -0400

    dot instead of source

--- ci/npm_lib.sh
+++ ci/npm_lib.sh
@@ -2,7 +2,7 @@
 set -e
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
+. $HOME/.cargo/env
 rustup toolchain install nightly
 rustup default stable
 rustup update
