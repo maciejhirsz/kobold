56e03dd4baffcc7b32987994d3efdc5a5f6e2b24
Maciej Hirsz <hello@maciej.codes>
2025-09-07 19:07:12 +0100

    Draw a thick cross if QRBuilder errors

--- crates/kobold_qr/src/lib.rs
+++ crates/kobold_qr/src/lib.rs
@@ -5,7 +5,7 @@ use std::fmt::Write;
 
 use kobold::prelude::*;
 
-use fast_qr::qr::QRBuilder;
+use fast_qr::{Module, qr::QRBuilder};
 use kobold::diff::fence;
 
 /// Error Correction Coding has 4 levels
@@ -37,55 +37,74 @@ impl Default for Ecl {
     }
 }
 
+// QR code sizes are calculated as 17 + N * 4, where N is in range of (1, 40),
+// 33 is therefore a valid size, but 32 is not and we can use it to mark error.
+const ERROR_SIZE: usize = 32;
+const ERROR_PATH: &str = "M4 0L0 4L12 16L0 28L4 32L16 20L28 32L32 28L20 16L32 4L28 0L16 12L4 0z";
+
 #[component(
     size?: 200,
     ecl?,
 )]
 pub fn qr(data: &str, size: usize, ecl: Ecl) -> impl View {
     fence(data, move || {
-        let qr = QRBuilder::new(data).ecl(ecl.into()).build().ok()?;
+        let (data, qrsize) = match QRBuilder::new(data).ecl(ecl.into()).build() {
+            Ok(qr) => (qr.data, qr.size),
+            Err(_) => ([Module(0); _], ERROR_SIZE),
+        };
 
-        let viewbox = format!("0 0 {} {}", qr.size, qr.size);
+        let view_box = format!("0 0 {} {}", qrsize, qrsize);
         let style = format!("width: {size}px; height: {size}px;");
+        let d = draw_path(data, qrsize);
 
-        // Most QR codes will generate a path with density of ~2.5 bytes per module,
-        // allocating 4 bytes per module should be more than sufficient.
-        let mut path = String::with_capacity(qr.data.len() * 4);
+        view! {
+            <svg {view_box} {style}>
+                <path {d} fill="currentColor">
+            </svg>
+        }
+    })
+}
 
-        for (y, row) in qr.data.chunks_exact(qr.size).enumerate() {
-            let row = &mut row.iter();
+fn draw_path<const N: usize>(data: [Module; N], size: usize) -> String {
+    // Most QR codes will generate a path with density of ~2.5 bytes per module,
+    // allocating 4 bytes per module should be more than sufficient.
+    let mut buf = String::with_capacity(size * size * 4);
 
-            // Find first filled module
-            let Some(x) = row.position(|m| m.value()) else {
-                continue;
+    if size == ERROR_SIZE {
+        buf.push_str(ERROR_PATH);
+        return buf;
+    }
+
+    for (y, row) in data.chunks_exact(size).take(size).enumerate() {
+        let row = &mut row.iter();
+
+        // Find first filled module
+        let Some(x) = row.position(|m| m.value()) else {
+            continue;
+        };
+
+        // Move to the new line
+        let _ = write!(&mut buf, "M{x} {y}");
+
+        loop {
+            // Find all horizontally adjacent filled modules
+            let width = row.take_while(|m| m.value()).count() + 1;
+
+            // `v1`       : draw line 1 segment down
+            // `h{width}` : draw line {width} segments right
+            // `v-1`      : draw line 1 segment up
+            let _ = write!(&mut buf, "v1h{width}v-1");
+
+            // Skip empty modules
+            let Some(empty) = row.position(|m| m.value()) else {
+                break;
             };
 
-            // Move to the new line
-            let _ = write!(&mut path, "M{x} {y}");
-
-            loop {
-                // Draw a recangle for all continous modules
-                let width = row.take_while(|m| m.value()).count() + 1;
-
-                let _ = write!(&mut path, "v1h{width}v-1");
-
-                // Skip empty modules
-                let Some(empty) = row.position(|m| m.value()) else {
-                    break;
-                };
-
-                // Note: we are drawing a line here (`hN`) instead of moving (`mN 0`) to
-                //       save some bytes, this is fine as long as we don't render line stroke.
-                let _ = write!(&mut path, "h{}", empty + 1);
-            }
+            // Note: we are drawing a line here (`h{}`) instead of moving (`m{} 0`) to
+            //       save some bytes, this is fine as long as we don't render line stroke.
+            let _ = write!(&mut buf, "h{}", empty + 1);
         }
+    }
 
-        Some(
-            view! {
-                <svg viewBox={viewbox} {style}>
-                    <path d={path} fill="currentColor">
-                </svg>
-            }
-        )
-    })
+    buf
 }
