7a90891082ae312e2988502cf08b4ad6d277c635
Maciej Hirsz <hello@maciej.codes>
2025-09-23 10:30:07 +0100

    Starting to fix the build process

--- examples/todomvc/bundle.sh
+++ examples/todomvc/bundle.sh
@@ -1,6 +1,6 @@
 # Rename the built dist file
 mv ./dist/kobold_todomvc_example.js ./dist/kobold_todomvc_example_large.js
 # Minimize the dist file into a new one
-node_modules/.bin/esbuild --bundle ./dist/kobold_todomvc_example_large.js --minify --outfile=./dist/kobold_todomvc_example.js --format=esm
+esbuild --bundle ./dist/kobold_todomvc_example_large.js --minify --outfile=./dist/kobold_todomvc_example.js --format=esm
 # Minimize wasm-bindgen import symbols
 cargo run --release --package kobold_cli -- dist/kobold_todomvc_example.js
